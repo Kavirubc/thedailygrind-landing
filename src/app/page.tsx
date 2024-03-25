fac2b357120a3384098bd9e69e6d2bc0b859f679
kaviru <hapuarachchikaviru@gmail.com>
2024-03-25 22:09:51 +0530

    logo and meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import { Herosec } from "@/components/herosec";
 import { Navbar } from "@/components/navbar";
 import Image from "next/image";
+import logo from "@/../public/hero1.png";
 
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Herosec />
+      <head>
+        <head>
+
+          <link rel="icon" href={logo.src} />
+        </head>
+      </head>
+      <body>
+        <Navbar />
+        <Herosec />
+
+      </body>
+
     </>
   );
 }
