850507de5c4cd0fa818db843348658b8a03854f0
kaviru <hapuarachchikaviru@gmail.com>
2024-03-25 21:48:21 +0530

    NewNav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import { FloatingNav } from "@/components/ui/floatingNavbar/page";
+import { Navbar } from "@/components/navbar";
 import Image from "next/image";
 
 
 export default function Home() {
   return (
     <>
-      <FloatingNav
-        navItems={[
-          { name: "Home", link: "#home" },
-          { name: "Join", link: "#join" },
-          
-        ]}
-      />
+      <Navbar />
+
     </>
   );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Navbar = () => {
+
+    return (
+        <div>
+            <nav className="border py-5 bg-slate-50 top-0">
+                <div className='px-16 flex flex-row justify-between'>
+                    <div>
+                        TDG
+                    </div>
+                    <div className='flex flex-row gap-x-10'>
+                        <div>
+                            Join Waitlist
+                        </div>
+                        <div>
+                            Luma
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+
