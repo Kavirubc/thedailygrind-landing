c19220034188728e4109a515f32955dcc9f7a155
kaviru <hapuarachchikaviru@gmail.com>
2024-03-26 16:25:41 +0530

    remove dots

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { Copyright } from 'lucide-react';
 
 export const Footer = () => {
     return (
-        <main className='flex flex-row min-h-[50px] gradient-background  justify-center items-center text-center px-4 sm:px-6 lg:px-8 bg-black text-white text-base'>
+        <main className='flex flex-row min-h-[50px]  justify-center items-center text-center px-4 sm:px-6 lg:px-8 bg-black text-white text-base'>
 
             <div className=''>
                 <Copyright
