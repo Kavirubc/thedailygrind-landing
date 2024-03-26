348fbf2920011bcc3c473e953d09d2bebf7999f3
kaviru <hapuarachchikaviru@gmail.com>
2024-03-26 13:35:40 +0530

    about me adjusted

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 export const Aboutme = () => {
     return (
-        <main className='flex flex-col min-h-screen justify-center items-center text-center px-4 sm:px-6 lg:px-8'>
+        <main className='flex flex-col min-h-screen justify-center items-center text-center px-4 sm:px-6 bg-violet-200 lg:px-8'>
             <div className='max-w-4xl w-full'>
                 <div className='flex flex-col items-center'>
                     <div className='w-1/2 md:w-1/2 lg:w-1/3 mb-8 lg:mb-0'>
@@ -21,7 +21,7 @@ export const Aboutme = () => {
                         </p>
                         <div className='flex flex-row py-4 justify-center lg:justify-start'>
                             <Link target='_blank' href='https://kh.ko-de.org/'>
-                                <button className='px-5 py-1 border border-black flex flex-row gap-x-1 hover:border-white hover:text-white hover:bg-black'>
+                                <button className='px-5 py-1 border border-black flex flex-row gap-x-1 hover:text-white hover:bg-violet-500'>
                                     Portfolio <IconArrowUpRight />
                                 </button>
                             </Link>
