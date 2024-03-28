8fb906fe251f5dafd23c50a83c88b6e85d561fa0
kaviru <hapuarachchikaviru@gmail.com>
2024-03-28 08:57:17 +0530

    add new page steps

---
+++ src/components/steps.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Steps = () => {
+    return (
+        <main className='flex bg-white flex-col min-h-screen justify-center items-center text-center px-4 sm:px-6 gradient-background lg:px-8'>
+            H
+        </main>
+    )
+}
+
+
