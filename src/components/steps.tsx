3a80fa711318763fd37fbe71d3cce2ecd690e567
kaviru <hapuarachchikaviru@gmail.com>
2024-03-28 09:40:37 +0530

    Mostly done

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -5,13 +5,15 @@ export const Steps = () => {
 
         <main className='flex bg-green-100 flex-col min-h-screen justify-center items-center text-center px-4 sm:px-6 md:px-8'>
             <div>
-                <h1 className='text-2xl sm:text-5xl font-bold'>
+                <h1 className='text-4xl sm:text-5xl font-bold'>
                     What we will be
-                    <span className='bg-black text-white pr-1'>Doing</span>
+                </h1>
+                <h1 className='text-4xl mt-2 sm:text-5xl font-bold bg-black text-white '>
+                    Doing
                 </h1>
             </div>
-            <div className='flex flex-col md:flex-row gap-y-8 md:gap-x-12 mt-12 md:mt-32 justify-around group'>
-                <div className='shadow-md lg:w-72 w-auto min-w-56 h-80 rounded-xl bg-slate-100 transition duration-300 ease-in-out transform hover:-translate-y-5 px-3 lg:px-0'>
+            <div className='flex flex-col md:flex-row gap-y-12 md:gap-x-12 mt-12 md:mt-32 justify-around group'>
+                <div className='shadow-md lg:w-72 w-auto min-w-48 lg:h-80 h-auto rounded-xl bg-slate-100 transition duration-300 ease-in-out transform hover:-translate-y-5 px-3 lg:px-0'>
                     <h1 className='text-2xl sm:text-3xl font-bold mt-8'>
                         Step 1
                     </h1>
@@ -19,7 +21,7 @@ export const Steps = () => {
                         Sign up for the event
                     </p>
                 </div>
-                <div className='shadow-md lg:w-72 w-auto min-w-56 h-80 rounded-xl bg-slate-100 transition duration-300 ease-in-out transform hover:-translate-y-5 px-3 lg:px-0'>
+                <div className='shadow-md lg:w-72 w-auto min-w-48 lg:h-80 h-auto rounded-xl bg-slate-100 transition duration-300 ease-in-out transform hover:-translate-y-5 px-3 lg:px-0'>
                     <h1 className='text-2xl sm:text-3xl font-bold mt-8'>
                         Step 2
                     </h1>
@@ -27,7 +29,7 @@ export const Steps = () => {
                         Attend the event
                     </p>
                 </div>
-                <div className='shadow-md lg:w-72 w-auto min-w-56 h-80 rounded-xl bg-slate-100 transition duration-300 ease-in-out transform hover:-translate-y-5 px-3 lg:px-0'>
+                <div className='shadow-md lg:w-72 w-auto min-w-48 lg:h-80 h-auto rounded-xl bg-slate-100 transition duration-300 ease-in-out transform hover:-translate-y-5 px-3 lg:px-0'>
                     <h1 className='text-2xl sm:text-3xl font-bold mt-8'>
                         Step 3
                     </h1>
