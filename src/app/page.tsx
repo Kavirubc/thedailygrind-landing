b64f47e8b79a196bf55762fe3864390e2e680670
kaviru <hapuarachchikaviru@gmail.com>
2024-03-29 08:33:39 +0530

    final push before new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         {/* <Herosecnew /> */}
         <Herosec />
         <About />
-        <Steps />
+        {/* <Steps /> */}
         <Aboutme />
         <Footer />
       </body>
