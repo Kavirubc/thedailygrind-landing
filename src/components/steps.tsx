05de4669b273f5bb8643d1ad6fadbd15e09fd9fb
kaviru <hapuarachchikaviru@gmail.com>
2024-03-31 01:36:11 +0530

    Steps updated

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -5,7 +5,7 @@ import steps from '@/../public/ActionPost.png'
 export const Steps = () => {
     return (
 
-        <main className="flex flex-col justify-center items-center text-center bg-white px-4 py-40 gradient-background sm:px-6 md:px-8">
+        <main className="flex flex-col justify-center items-center text-center bg-white px-4 pb-40 gradient-background sm:px-6 md:px-8">
             <div>
                 <h1 className="font-bold text-4xl sm:text-5xl">
                     What we will be
@@ -14,7 +14,7 @@ export const Steps = () => {
                     Doing
                 </h1>
             </div>
-            <div className='w-1/2 md:w-1/2 lg:w-1/3 mb-8 lg:mb-0'>
+            <div className='w-2/3 md:w-1/2 lg:w-1/3 mt-10 lg:mt-0 '>
                 <Image className='rounded-xl shadow-lg' src={steps} alt='Hero' />
             </div>
             {/* <div className="flex flex-col gap-y-12 mt-32 md:flex-row md:gap-x-12 justify-around group">
