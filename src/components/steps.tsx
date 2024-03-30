9d2a23659a84fae7ff2bc3a4381d3e1103e3dd46
kaviru <hapuarachchikaviru@gmail.com>
2024-03-31 01:40:44 +0530

    Done

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -7,15 +7,15 @@ export const Steps = () => {
 
         <main className="flex flex-col justify-center items-center text-center bg-white px-4 pb-40 gradient-background sm:px-6 md:px-8">
             <div>
-                <h1 className="font-bold text-4xl sm:text-5xl">
+                <h1 className="font-bold text-4xl sm:text-4xl">
                     What we will be
                 </h1>
-                <h1 className="mt-2 font-bold text-4xl text-black py-1 bg-yellow-200/70 sm:text-5xl">
+                <h1 className="mt-2 font-bold text-4xl text-black py-1 bg-yellow-200/70 shadow-lg rounded-lg sm:text-5xl">
                     Doing
                 </h1>
             </div>
-            <div className='w-2/3 md:w-1/2 lg:w-1/3 mt-10 lg:mt-0 '>
-                <Image className='rounded-xl shadow-lg' src={steps} alt='Hero' />
+            <div className='w-2/3 md:w-1/3 lg:w-1/3 mt-10 lg:mt-20 '>
+                <Image className='rounded-xl shadow-lg shadow-cyan-100' src={steps} alt='Hero' />
             </div>
             {/* <div className="flex flex-col gap-y-12 mt-32 md:flex-row md:gap-x-12 justify-around group">
                 <div className="transition duration-300 ease-in-out transform hover:-translate-y-3 shadow-md w-auto min-w-48 lg:w-72 h-auto lg:h-80 rounded-xl bg-gray-100 px-3 lg:px-0">
