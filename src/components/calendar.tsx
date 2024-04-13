ee16d7c8e58b4d8e9cbe994c94a5cac7b0644b8f
kaviru <hapuarachchikaviru@gmail.com>
2024-04-13 08:53:35 +0530

    lu.ma added

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const Calendar = () => {
     return (
         <main className="flex flex-col justify-center items-center text-center bg-white px-4 pb-40 gradient-background sm:px-6 md:px-8">
+            <h1 className='text-4xl sm:text-5xl font-bold pb-10'>
+                Subscribe us on <br /><span className='bg-black text-white text-3xl'>LU.MA</span>
+            </h1>
             <iframe
+            className='rounded-xl shadow-lg shadow-cyan-100 max-w-sm md:max-w-md lg:max-w-5xl'
                 src="https://lu.ma/embed/calendar/cal-eIHkrf3p9WieHOL/events?lt=light"
                 width="700"
                 height="450"
