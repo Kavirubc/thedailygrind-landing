062f155f13eb72785bf768fb0b19b7bb668c9526
kaviru <hapuarachchikaviru@gmail.com>
2024-05-05 10:48:23 +0530

    Join waitlist added

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -7,6 +7,8 @@ import { useUser } from "@clerk/nextjs";
 
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { User } from "lucide-react";
+import Link from "next/link";
+import { IconArrowUpRight } from "@tabler/icons-react";
 
 
 
@@ -27,10 +29,13 @@ export default function Home() {
                     <br />
                     Coming soon
 
-                    <div>
-                        <SignedIn />
-                        <SignedOut />
-                    </div>
+                    <button className="px-5 py-1 border items-center border-black flex flex-row gap-x-1 hover:border-violet-600 hover:text-white hover:bg-purple-pink-gradient mt-5 text-center">
+                        <Link href='/community/join'>
+                            <div className=' px-4 py-2 md:py-0 flex flex-row' >
+                                Waitlist  <IconArrowUpRight height={13} />
+                            </div>  
+                        </Link>
+                    </button>
                 </main>
             </>
         );
@@ -43,11 +48,20 @@ export default function Home() {
                 Welcome to community..
                 <br />
                 Please sign in for more details..
-                <SignInButton mode="modal" fallbackRedirectUrl="/community">
-                    <button className="mt-5 border border-secondary bg-white px-4 py-1.5 text-base hover:text-white hover:bg-black">
-                        <span>Sign in</span>
+                <div className="flex flex-row gap-x-5">
+                    <SignInButton mode="modal" fallbackRedirectUrl="/community">
+                        <button className="mt-5 border border-secondary bg-white px-4 py-1.5 text-base hover:text-white hover:bg-black">
+                            <span>Sign in</span>
+                        </button>
+                    </SignInButton>
+                    <button className="px-5 py-1 border items-center border-black flex flex-row gap-x-1 hover:border-violet-600 hover:text-white hover:bg-purple-pink-gradient mt-5 text-center">
+                        <Link href='/community/join'>
+                            <div className=' px-4 py-2 md:py-0 flex flex-row' >
+                                Waitlist  <IconArrowUpRight height={13} />
+                            </div>
+                        </Link>
                     </button>
-                </SignInButton>
+                </div>
             </main>
         </>
     );
