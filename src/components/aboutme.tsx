48f155c842d8584101d06336640d730f878d5262
kaviru <hapuarachchikaviru@gmail.com>
2024-06-17 11:59:53 +0530

    Updated code

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import pro from '@/../public/a.png';
+import pro2 from '@/../public/danidu.jpeg';
 import { IconArrowUpRight } from "@tabler/icons-react";
 import Link from 'next/link';
 import { Ghost, Handshake } from 'lucide-react';
@@ -8,29 +9,52 @@ import { Ghost, Handshake } from 'lucide-react';
 export const Aboutme = () => {
     return (
         <main className='flex flex-col min-h-screen justify-center items-center text-center px-4 sm:px-6 bg-violet-100 lg:px-8'>
-            <div className='max-w-4xl w-full'>
-                <div className='flex flex-col items-center'>
-                    <div className='w-1/2 md:w-1/2 lg:w-1/3 mb-8 lg:mb-0'>
-                        <Image className='rounded-xl shadow-lg' src={pro} alt='Hero' />
+            <h1 className='text-3xl sm:text-5xl font-bold mb-36'>
+                About <span className='bg-black text-white px-2'>us</span>
+            </h1>
+            <div className='max-w-4xl w-full '>
+                <div className='flex flex-col md:flex-row items-center justify-center space-y-12 md:space-y-0 md:space-x-12 gap-x-20'>
+                    <div className='flex flex-col'>
+                        <div className=' mb-8 lg:mb-0'>
+                            <Image className='rounded-xl shadow-lg' height={200} src={pro} alt='Hero' />
+                        </div>
+                        <div className='lg:mt-6 text-center lg:text-left'>
+                            <p className='font-semibold text-lg'>Kaviru Hapuarachchi</p>
+                            <p className='text-base max-w-xs mb-5 mt-2'>
+                                Productivity Nerd working with NextJS
+                            </p>
+                            <div className='flex flex-row py-4 justify-center lg:justify-start'>
+                                <Link target='_blank' href='https://kh.ko-de.org/'>
+                                    <button className='px-5 py-1 border border-black flex items-center flex-row gap-x-1 hover:text-white hover:bg-violet-600'>
+                                        Let's talk <Handshake height={18} />
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
-                    <div className='lg:mt-10 lg:text-left'>
-                        <h1 className='text-3xl sm:text-5xl font-bold'>
-                            About <span className='bg-black text-white'>me</span>
-                        </h1>
-                        <p className='text-base max-w-2xl mb-5 mt-5' >
-                            Productivity Nerd working with NextJS
-                        </p>
-                        <div className='flex flex-row py-4 justify-center lg:justify-start'>
-                            <Link target='_blank' href='https://kh.ko-de.org/'>
-                                <button className='px-5 py-1 border border-black flex items-center flex-row gap-x-1 hover:text-white hover:bg-violet-600'>
-                                    View Portfolio <Handshake height={18} />
-                                </button>
-                            </Link>
+                    <div className='flex flex-col'>
+                        <div className=' mb-8 lg:mb-0'>
+                            <Image className='rounded-xl shadow-lg' height={200} src={pro2} alt='Hero' />
+                        </div>
+                        <div className='lg:mt-6 text-center lg:text-left'>
+                            <p className='font-semibold text-lg'>Danidu Nanayakkara</p>
+                            <p className='text-base max-w-xs mb-5 mt-2'>
+                                Productivity Nerd working with NextJS
+                            </p>
+                            <div className='flex flex-row py-4 justify-center lg:justify-start'>
+                                <Link target='_blank' href='https://linktr.ee/danidun'>
+                                    <button className='px-5 py-1 border border-black flex items-center flex-row gap-x-1 hover:text-white hover:bg-violet-600'>
+                                        Let's talk <Handshake height={18} />
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </main>
+
+
     );
 }
 
